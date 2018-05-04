a3fcfb621d69707ed1a1864d0b59b4837eed97e3
jsc215 <jeffcomertennis@gmail.com>
2018-05-04 00:32:09 -0400

    add css

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-let socket = io();
+const socket = io();
 
 socket.on('connect', () => {
   console.log('connected to server');
@@ -29,10 +29,13 @@ socket.on('newLocationMessage', (message) => {
 $('#message-form').on('submit', (e) => {
   e.preventDefault();
 
+  let messageTextbox = $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextbox.val()
   }, () => {
+    messageTextbox.val('');
   })
 })
 
@@ -41,12 +44,17 @@ locationButton.on('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
   navigator.geolocation.getCurrentPosition((position) => {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, () => {
+    locationButton.removeAttr('disabled').text('Send Location');
     alert('Unable to fetch location');
   })
 })
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-const path = require('path');
-const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const { generateMessage, generateLocationMessage } = require('./utils/message');
+const http = require('http');
+const path = require('path');
 
-const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 app.use(express.static(publicPath));
 
@@ -28,7 +29,7 @@ io.on('connection', socket => {
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('this is from the server');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
