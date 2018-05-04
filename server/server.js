7b2258511c3e7f18dd335ea5826a62c50c87e14a
jsc215 <jeffcomertennis@gmail.com>
2018-05-04 16:40:24 -0400

    messages now send to only people in same chat room

--- server/server.js
+++ server/server.js
@@ -14,7 +14,6 @@ const users = new Users();
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
@@ -51,25 +50,45 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    let user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io
+        .to(user.room)
+        .emit('newMessage', generateMessage(user.name, message.text));
+    }
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit(
-      'newLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude)
-    );
+    let user = users.getUser(socket.id);
+
+    if (user) {
+      io
+        .to(user.room)
+        .emit(
+          'newLocationMessage',
+          generateLocationMessage(
+            `${user.name}`,
+            coords.latitude,
+            coords.longitude
+          )
+        );
+    }
   });
 
   socket.on('disconnect', () => {
-   let user = users.removeUser(socket.id);
+    let user = users.removeUser(socket.id);
 
-   if (user) {
-     io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-     io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left the room`));
-   }
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io
+        .to(user.room)
+        .emit(
+          'newMessage',
+          generateMessage('Admin', `${user.name} has left the room`)
+        );
+    }
   });
 });
 
